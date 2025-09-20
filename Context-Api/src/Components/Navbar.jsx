ccbbe59207d2cdbd2cc533e1205765a85dac9e35
Bhumika <bhumika04shimla@gmail.com>
2025-09-20 14:25:36 +0530

    done styling for navbar

--- Context-Api/src/Components/Navbar.jsx
+++ Context-Api/src/Components/Navbar.jsx
@@ -1,26 +1,48 @@
+import styles from 'Navbar.module.css'
+
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className={`${styles.navbar} navbar navbar-expand-lg bg-body-tertiary`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+          <a className={`${styles['navbar-brand']} navbar-brand`} href="#">Navbar</a>
+
+          <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+           aria-controls="navbarNav" 
+           aria-expanded="false" 
+           aria-label="Toggle navigation">
+
             <span className="navbar-toggler-icon"></span>
+
           </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
+
           <ul className="navbar-nav">
+
             <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+
+                <a className={`${styles.navLink} nav-link active`} aria-current="page" href="#">Home</a>
+
             </li>
+
             <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
+                <a className={`${styles.navLink} nav-link`} href="#">Features</a>
             </li>
+
             <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
+                <a className={`${styles.navLink} nav-link`} href="#">Pricing</a>
             </li>
+
             <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                <a className={`${styles.navLink} nav-link disabled`} aria-disabled="true">Disabled</a>
             </li>
+
           </ul>
         </div>
        </div>
